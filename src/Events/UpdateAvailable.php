018dc9097e1e8157c40b5cfcdd4d0005c784d8d3
Holger Lösken <holger.loesken@codedge.de>
2016-08-03 23:16:10 +0200

    Add new event for update available

---
+++ src/Events/UpdateAvailable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Codedge\Updater\Events;
+
+use App\Events\Event;
+use Codedge\Updater\Contracts\SourceRepositoryTypeContract;
+
+/**
+ * UpdateFailed.php.
+ *
+ * @author Holger Lösken <holger.loesken@codedge.de>
+ * @copyright See LICENSE file that was distributed with this source code.
+ */
+class UpdateAvailable extends Event
+{
+    /**
+     * @var  SourceRepositoryTypeContract
+     */
+    protected $sourceRepository;
+
+    /**
+     * UpdateFailed constructor.
+     *
+     * @param SourceRepositoryTypeContract $sourceRepository
+     */
+    public function __construct(SourceRepositoryTypeContract $sourceRepository)
+    {
+        $this->sourceRepository = $sourceRepository;
+    }
+}
