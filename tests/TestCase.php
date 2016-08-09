d1b64d4d7f0f7b2c6560e8510993eef1acd51742
Holger Lösken <holger.loesken@codedge.de>
2016-08-10 00:00:33 +0200

    Update base test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,45 @@
 
 namespace Codedge\Updater\Tests;
 
+use GuzzleHttp\Client;
 use Illuminate\Foundation\Application;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    /**
+     * @var array
+     */
+    protected $config;
+
+
+    protected $client;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->config = [
+            'default' => 'github',
+            'repository_types' => [
+                'github' => [
+                    'type' => 'github',
+                    'repository_vendor' => 'laravel',
+                    'repository_name' => 'laravel',
+                    'repository_url' => '',
+                    'download_path' => '/tmp',
+                ],
+            ],
+            'log_events' => false,
+            'mail_to' => [
+                'address' => '',
+                'name' => '',
+            ],
+        ];
+
+        $this->client = new Client();
+    }
+
     /**
      * @param Application $app
      * @return array
@@ -14,7 +48,7 @@ class TestCase extends BaseTestCase
     protected function getPackageProviders($app)
     {
         return [
-            Codedge\Updater\UpdaterServiceProvider::class,
+            'Codedge\Updater\UpdaterServiceProvider',
         ];
     }
 
@@ -25,7 +59,7 @@ class TestCase extends BaseTestCase
     protected function getPackageAliases($app)
     {
         return [
-            'Updater' => Codedge\Updater\UpdaterFacade::class,
+            'Updater' => 'Codedge\Updater\UpdaterFacade',
         ];
     }
 }
