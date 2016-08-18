addbabb4f317eee7900320cb5b3ca60983163fc8
Holger Lösken <holger.loesken@codedge.de>
2016-08-18 23:34:11 +0200

    If no version specified for fetching/updating then use latest version avail

--- src/SourceRepository.php
+++ src/SourceRepository.php
@@ -36,6 +36,8 @@ class SourceRepository implements SourceRepositoryTypeContract
      */
     public function fetch($version = '')
     {
+        $version = !empty($version) ? $version : $this->getVersionAvailable();
+
         return $this->sourceRepository->fetch($version);
     }
 
@@ -49,6 +51,8 @@ class SourceRepository implements SourceRepositoryTypeContract
      */
     public function update($version = '', $forceFetching = true)
     {
+        $version = !empty($version) ? $version : $this->getVersionAvailable();
+
         if ($forceFetching) {
             $this->fetch($version);
         }
