47cd5d101cefc8de7425d9f06cd46821a1034d69
Holger Lösken <hl@inquence.com>
2020-03-02 23:05:17 +0100

    Fix docblock

--- src/SourceRepositoryTypes/HttpRepositoryType.php
+++ src/SourceRepositoryTypes/HttpRepositoryType.php
@@ -208,7 +208,7 @@ class HttpRepositoryType extends AbstractRepositoryType implements SourceReposit
      * @param string $prepend Prepend a string to the latest version
      * @param string $append Append a string to the latest version
      *
-     * @return string|false
+     * @return string
      * @throws Exception
      */
     public function getVersionAvailable($prepend = '', $append = ''): string
